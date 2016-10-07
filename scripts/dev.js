9a6f86a876b2be5a827b7729d61c09ac6c4eefb2
bouare12 <bandiougoubouare@yahoo.fr>
2016-10-07 23:43:20 +0200

    devjs

---
+++ scripts/dev.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+         $("#menu > ul > li").hover(
+          function(){ $(this).find("ul").slideDown('fast'); } ,
+  function(){ $(this).find("ul").slideUp('fast'); } );
+ }
